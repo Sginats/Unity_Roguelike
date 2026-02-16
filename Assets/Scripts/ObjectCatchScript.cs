0e711a1dc8986f5917211cd9f21b4bd16db7d83b
Sginats <senvaldsniks2008@gmail.com>
2026-02-16 16:04:32 +0200

    Uzlabots Object Catch script lai varētu ķert jaunos prefab un atņemt punktus

--- Assets/Scripts/ObjectCatchScript.cs
+++ Assets/Scripts/ObjectCatchScript.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ObjectCatchScript : MonoBehaviour
 {
@@ -7,26 +7,36 @@ public class ObjectCatchScript : MonoBehaviour
     private Rigidbody2D rb;
     SFX_Script sfx;
 
-
     void Start()
     {
         sfx = FindFirstObjectByType<SFX_Script>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.IsChildOf(transform))
             return;
-        
-        if(collision.CompareTag("Donut")) {
-            sfx.PlaySFX(4);
+
+        if (collision.CompareTag("Good"))
+        {
+
+            if (sfx != null) sfx.PlaySFX(4);
+
             Destroy(collision.gameObject);
+
             transform.localScale += new Vector3(sizeIncrease, sizeIncrease, 0);
             rb.mass += massIncrease;
-        
-        } else
-            Debug.Log("Collided with non-donut object: " + collision.gameObject.name);
+        }
+
+        else if (collision.CompareTag("Bad"))
+        {
+            Debug.Log("Trāpīja sliktajam: " + collision.gameObject.name);
+
+        }
+        else
+        {
+            Debug.Log("Sadursme ar objektu bez taga: " + collision.gameObject.name);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,98 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("UI Elementi")]
+    public TMP_Text scoreText;
+    public TMP_Text timerText;
+    public TMP_Text healthText;
+    public GameObject gameOverPanel;
+
+    [Header("Spēles Iestatījumi")]
+    public int maxHealth = 3;
+    private int currentHealth;
+    private int score = 0;
+    private float timer = 0f;
+    private bool isGameActive = true;
+
+    [Header("Skaņas (Indeksi no SFX saraksta)")]
+    public int catchSoundIndex = 0;
+    public int hitSoundIndex = 1; 
+    public int deathSoundIndex = 4;
+
+    SFX_Script sfx;
+
+    void Start()
+    {
+        sfx = FindFirstObjectByType<SFX_Script>();
+        currentHealth = maxHealth;
+        UpdateUI();
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (isGameActive)
+        {
+            timer += Time.deltaTime;
+            if (timerText != null)
+                timerText.text = "Laiks: " + timer.ToString("F2") + "s";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isGameActive) return;
+
+        if (collision.CompareTag("Good"))
+        {
+            score++;
+            if (sfx != null) sfx.PlaySFX(catchSoundIndex);
+            Destroy(collision.gameObject);
+        }
+
+        else if (collision.CompareTag("Bad"))
+        {
+            Destroy(collision.gameObject); 
+            TakeDamage();
+        }
+
+        UpdateUI();
+    }
+
+    void TakeDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            GameOver();
+        }
+        else
+        {
+            if (sfx != null) sfx.PlaySFX(hitSoundIndex);
+        }
+    }
+
+    void GameOver()
+    {
+        isGameActive = false;
+
+        if (sfx != null) sfx.PlaySFX(deathSoundIndex);
+
+        Time.timeScale = 0;
+
+        if (healthText != null) healthText.text = "Dzīvības: 0";
+        if (gameOverPanel != null) gameOverPanel.SetActive(true);
+    }
+
+    void UpdateUI()
+    {
+        if (scoreText != null) scoreText.text = "Punkti: " + score;
+        if (healthText != null) healthText.text = "Dzīvības: " + currentHealth;
+    }
+}
